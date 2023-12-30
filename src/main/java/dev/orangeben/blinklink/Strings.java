4e3a059d0082630caa1b1f58b6ce50bc5d52fc7f
Benjamin-C <bencrall@gmail.com>
2023-12-29 18:06:44 -0800

    Changed stick name

--- src/main/java/dev/orangeben/blinklink/Strings.java
+++ src/main/java/dev/orangeben/blinklink/Strings.java
@@ -2,7 +2,7 @@ package dev.orangeben.blinklink;
 
 public class Strings {
     
-    public static final String TPSTICK_NAME = "BlinkLink Setup Stick";
+    public static final String TPSTICK_NAME = "BlinkLink Linking Stick";
     public static final String TPSTICK_ID_KEY = "BlinkLinkID";
 
 }
